13f5f190e3ff9ef4524ff184357e7b6bf7f6bcc0
Ramiro Berrelleza <rberrelleza@gmail.com>
2018-10-18 16:20:59 +0200

    harcode version

--- Formula/cnd.rb
+++ Formula/cnd.rb
@@ -1,8 +1,7 @@
 class Cnd < Formula
     desc "CLI for cloud native development"
     homepage "https://github.com/okteto/cnd"
-    version "0.1.0"
-    url "https://github.com/okteto/cnd/archive/#{version}.tar.gz"
+    url "https://github.com/okteto/cnd/archive/0.1.0.tar.gz"
     sha256 "edb31ebac68d15541d15bc8fa0b1307b5f3e5c5cfd2a54f1c69ca45f1d880339"
     head "https://github.com/okteto/cnd.git"
 
@@ -14,12 +13,12 @@ class Cnd < Formula
         bin_path = buildpath/"src/github.com/okteto/cnd"
         bin_path.install Dir["*"]
         cd bin_path do
-            system "go", "build", "-ldflags", "-X github.com/okteto/cnd/cmd.VersionString=#{version}", "-o", bin/"cnd", "."
+            system "go", "build", "-ldflags", "-X github.com/okteto/cnd/cmd.VersionString=0.1.0", "-o", bin/"cnd", "."
         end
     end
 
     # Homebrew requires tests.
     test do
-        assert_match "cnd version #{version}", shell_output("#{bin}/cnd version 2>&1", 0)
+        assert_match "cnd version 0.1.0", shell_output("#{bin}/cnd version 2>&1", 0)
     end
 end
