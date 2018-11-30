893ea29cfa2f83139547e1e112fb928d1b688944
Ramiro Berrelleza <rberrelleza@gmail.com>
2018-11-30 15:40:05 -0800

    release-0.1.1

--- Formula/cnd.rb
+++ Formula/cnd.rb
@@ -1,8 +1,8 @@
 class Cnd < Formula
     desc "CLI for cloud native development"
     homepage "https://github.com/okteto/cnd"
-    url "https://github.com/okteto/cnd/releases/download/0.1.0/cnd-darwin-amd64"
-    sha256 "e7808ef76afcba28e1a21142be83abf41935f24bf658c6176d6603ae82d27fe6"
+    url "https://github.com/okteto/cnd/releases/download/0.1.1/cnd-darwin-amd64"
+    sha256 "f3a8f3e705e734633c2c0c06bd09fc1628503d384868525313cec933460146bd"
     version "0.1.0"
     depends_on "syncthing"
 
@@ -13,6 +13,6 @@ class Cnd < Formula
 
     # Homebrew requires tests.
     test do
-        assert_match "cnd version 0.1.0", shell_output("#{bin}/cnd version 2>&1", 0)
+        assert_match "cnd version 0.1.1", shell_output("#{bin}/cnd version 2>&1", 0)
     end
 end
