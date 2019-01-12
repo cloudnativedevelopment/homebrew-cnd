878f6284b9e18d9855208816b6c5e02167b4a2c2
okteto <ci@okteto.com>
2019-01-12 13:32:28 +0000

    0.4.3 release

--- Formula/cnd.rb
+++ Formula/cnd.rb
@@ -2,8 +2,8 @@ class Cnd < Formula
     desc "CLI for cloud native development"
     homepage "https://github.com/okteto/cnd"
     url "https://github.com/okteto/cnd.git",
-      :tag      => "0.4.2",
-      :revision => "afd3b137b6d8018df3242f6f1ec4132579b8e4f2"
+      :tag      => "0.4.3",
+      :revision => "4b859508cdcbeb10515aba94ea8ea367c2036fa5"
     head "https://github.com/okteto/cnd.git"
 
     depends_on "syncthing"
@@ -11,7 +11,7 @@ class Cnd < Formula
 
     def install
       ENV["GOPATH"] = buildpath
-      ENV["VERSION_STRING"] = "0.4.2"
+      ENV["VERSION_STRING"] = "0.4.3"
       contents = Dir["{*,.git,.gitignore}"]
       (buildpath/"src/github.com/okteto/cnd").install contents
       cd "src/github.com/okteto/cnd" do
