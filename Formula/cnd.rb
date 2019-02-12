cd4becc1a2dbba95414587d41b450f09b2e48457
okteto <ci@okteto.com>
2019-02-12 21:18:48 +0000

    0.4.6 release

--- Formula/cnd.rb
+++ Formula/cnd.rb
@@ -2,15 +2,15 @@ class Cnd < Formula
     desc "CLI for cloud native development"
     homepage "https://github.com/cloudnativedevelopment/cnd"
     url "https://github.com/cloudnativedevelopment/cnd.git",
-      :tag      => "0.4.5",
-      :revision => "65b331aad6f858a7ea6ceae3daa286c47d710bc7"
+      :tag      => "0.4.6",
+      :revision => "fe93a3ef3a13979a99e46d6a758ba70053ea6352"
     head "https://github.com/cloudnativedevelopment/cnd.git"
 
     depends_on "go" => :build
 
     def install
       ENV["GOPATH"] = buildpath
-      ENV["VERSION_STRING"] = "0.4.5"
+      ENV["VERSION_STRING"] = "0.4.6"
       contents = Dir["{*,.git,.gitignore}"]
       (buildpath/"src/github.com/cloudnativedevelopment/cnd").install contents
       cd "src/github.com/cloudnativedevelopment/cnd" do
